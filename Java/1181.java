fb2f68b9ff22fc606bd1dee5f882c5e18d2baad1
Hamtoto <gohg0901@naver.com>
2023-09-20 21:27:28 +0900

    1181 해결

--- Java/1181.java
+++ Java/1181.java
@@ -1,53 +1,30 @@
 import java.io.*;
 import java.util.*;
 
-class WordSort {
-    private static void quickSort(int[] arr, int start, int end) {
-        int part = partition(arr, start, end);
-        if (start < part - 1) quickSort(arr, start, part - 1);
-        if (end > part) quickSort(arr, part, end);
-    }
-
-    private static int partition(int[] arr, int start, int end) {
-        int pivot = arr[(start + end) / 2];
-        while (start <= end) {
-            while (arr[start] < pivot) start++;
-            while (arr[end] > pivot) end--;
-            if (start <= end) {
-                swap(arr, start, end);
-                start++;
-                end--;
-            }
-        }
-        return start;
-    }
-
-    private static void swap(int[] arr, int start, int end) {
-        int tmp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = tmp;
-        return;
-    }
-
-    public static void main(String args[]) throws IOException {
-        String[][] arr = null;
-
+class Main {
+	public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int line = Integer.parseInt(br.readLine());
+     
+        int num = Integer.parseInt(br.readLine());
+        String[] arr = new String[num];
 
-        arr = new String[line][2];
-
-        for (int i = 0; i < line; i++) {
-            arr[i][0] = br.readLine();
-            arr[i][1] = Integer.toString(arr[i][0].length());
+        for(int i =  0 ; i < num ; i++) {
+            arr[i] = br.readLine();
         }
 
-        quickSort(arr, 0, line - 1);
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + ",");
-        }
-
-
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if(s1.length() == s2.length()) return s1.compareTo(s2);
+                else return s1.length() - s2.length();
+            }
+        });
+	   
+	   	System.out.println(arr[0]);
+	 	for(int i = 1; i < num; i++) {
+   		    if (!arr[i].equals(arr[i - 1])) {
+        	    System.out.println(arr[i]);
+		    }
+	    }
     }
 }
