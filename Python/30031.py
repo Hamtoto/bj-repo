dc5780112558b282ce36f17beb26b8e8f3d65bf9
Hamtoto <gohg0901@naver.com>
2023-10-14 02:07:44 +0900

    30031 해결

---
+++ Python/30031.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+sum = 0
+
+for i in range(0,n):
+    a, b = map(int, input().split())
+
+    if a == 136:
+        sum += 1000
+    elif a == 142:
+        sum += 5000
+    elif a == 148:
+        sum += 10000
+    elif a == 154:
+        sum += 50000
+print(sum)
