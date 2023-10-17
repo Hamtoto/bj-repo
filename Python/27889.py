cb93ea5e6a0b1f55acde39e290fd1e423677afcc
Hamtoto <gohg0901@naver.com>
2023-10-18 01:26:37 +0900

    27889 해결

---
+++ Python/27889.py
@@ -0,0 +1,9 @@
+text = str(input())
+if(text == "NLCS"):
+    print("North London Collegiate School")
+elif(text == "BHA"):
+    print("Branksome Hall Asia")
+elif(text == "KIS"):
+    print("Korea International School")
+elif(text == "SJA"):
+    print("St. Johnsbury Academy")
