d40a505a8c1428d9ed292440e01c66ed71860aeb
Hamtoto <gohg0901@naver.com>
2023-11-27 16:22:48 +0900

    30402 해결

---
+++ Python/30402.py
@@ -0,0 +1,12 @@
+for i in range(0,15):
+    arr = list(map(str, input().split()))
+
+    if 'w' in arr:
+        print("chunbae")
+        break
+    elif 'b' in arr:
+        print("nabi")
+        break
+    elif 'g' in arr:
+        print("yeongcheol")
+        break
