393a4dc7dd82b4a060dae617421679e9dd4f78a0
Hamtoto <gohg0901@naver.com>
2023-12-10 02:04:32 +0900

    30676 해결

---
+++ Python/~40000/30676.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+if 620 <= n and n <= 780:
+    print("Red")
+elif 590 <= n and n < 620:
+    print("Orange")
+elif 570 <= n and n < 590:
+    print("Yellow")
+elif 495 <= n and n < 570:
+    print("Green")
+elif 450 <= n and n < 495:
+    print("Blue")
+elif 425 <= n and n < 450:
+    print("Indigo")
+elif 380 <= n and n < 425:
+    print("Violet")
