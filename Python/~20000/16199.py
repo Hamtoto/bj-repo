3e23a14594f87838056361dace255672314a86f5
Hamtoto <gohg0901@naver.com>
2023-12-21 05:02:56 +0900

    16199 해결

---
+++ Python/~20000/16199.py
@@ -0,0 +1,21 @@
+y1, m1, d1 = map(int, input().split())
+y2, m2, d2 = map(int, input().split())
+
+man = 0
+
+if m1 < m2:
+    man = y2-y1
+elif m1 == m2:
+    if d1 <= d2:
+        man = y2-y1
+    else:
+        man = y2-y1-1
+else:
+    man = y2-y1-1
+
+count = y2-y1+1
+year = y2-y1
+
+print(man)
+print(count)
+print(year)
